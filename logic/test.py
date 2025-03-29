9ee87d27dd84157e30d263d199334d4698970c44
ProudRykar <proudrykar@mail.ru>
2025-03-29 18:13:54 +0300

    [+] Комментарии-инструкции по добавлению рун в эдитор/терминал

--- logic/test.py
+++ logic/test.py
@@ -1,4 +1,8 @@
 # Файл для теста кодировок и иных проблем эдитора/терминала
+# Для поддержки рун закинуть в settings.json:
+# "editor.fontFamily": "'Segoe UI Historic', 'DejaVu Sans', 'Fira Code', monospace"
+# "terminal.integrated.fontFamily": "'Segoe UI Historic', 'DejaVu Sans', 'Fira Code', monospace",
+
 import unicodedata
 
 runes = "ᚠᚢᚦᚨᛚᛏ"
